dbe0db8c4fe47cc4853b8f7f48e3937047bcde0e
Curti <curtisbotonis@gmail.com>
2024-06-24 17:17:09 -0700

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using MathGame;
+using Microsoft.VisualBasic;
+using static System.Formats.Asn1.AsnWriter;
+
+var menu = new Menu();
+
+var date = DateTime.UtcNow;
+
+var games = new List<string>();
+
+string name = Helpers.GetName();
+
+
+
+menu.ShowMenu(name, date);
+
+
+
+
+
+
+
+
+
+
+
